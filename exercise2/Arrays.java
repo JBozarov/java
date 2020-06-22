165183d117683e251439bdbb1cbc1aa452acdd55
John Bozarov <codinginusa@gmail.com>
2020-06-22 14:34:13 -0400

    Exercise 2 is done

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -1,14 +1,30 @@
-package exercise2;
+package com.exercise2;
+
+
 
 class Arrays {
     //Take the arrays.js file, and refactor the code into Java in this file.
-    int[] nums;
-    String[] greetings;
-    boolean[] binaries;
+    int[] nums = {10,23,3,4,5,2,1};
+    String[] greetings = {"hello","howdy","hallo","hola","bonjour","ciao","أهلا"};
+    boolean[] binaries = {false, true, true, true, false, false, false, true, true, true, true, false, true, false, false, true, false};
 
-    void printNums() {};
-    void printGreetings() {};
-    void printBinaries() {};
+    void printNums() {
+        for ( int i = 0; i<nums.length; i++ ) {
+            System.out.println("Number values are: " + nums[i]);
+        }
+    };
+
+
+    void printGreetings() {
+        for (int k = 0; k<greetings.length; k++ ) {
+            System.out.println("String values are: " + greetings[k]);
+        }
+    };
+    void printBinaries() {
+        for ( int m = 0; m<binaries.length; m++ ) {
+            System.out.println("Boolean values are: " + binaries[m]);
+        }
+    };
 }
 
 
--- exercise2/Colors.java
+++ exercise2/Colors.java
@@ -1,16 +1,29 @@
 package exercise2;
 
-public class Colors {
+import java.util.ArrayList;
 
+public class Colors {
+    public static ArrayList<String> myList = new ArrayList<String>();
     String[] colorful(String[] strings) {
-        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.) return an array of strings that start with a color.  discard all the other strings.  If no strings start with a color in the input array, return an empty array.
-        return new String[]{};
+        for ( int i = 0; i<strings.length; i++ ) {
+            String[] arr = strings[i].split(" ");
+            if ( arr[0].equals("red") || arr[0].equals("yellow") || arr[0].equals("green") || arr[0].equals("black") || arr[0].equals("pink")) {
+                myList.add(strings[i]);
+            }
+        }
+        return strings;
     }
 
     public static void main(String[] args) {
         Colors code = new Colors();
-        code.colorful(new String[]{"bluebird","red herring", "yellow bumblebee","black spider", "green monkey","pink","great gatsby","feeding frenzy","black bird","widowmaker","quran","bible","torah"});
+        String[] colors = {"bluebird","red herring", "yellow bumblebee","black spider", "green monkey","pink","great gatsby","feeding frenzy","black bird","widowmaker","quran","bible","torah"};
+        code.colorful(colors);
+
+
         System.out.println("This is the Colors class");
+        for ( int i = 0; i<myList.size(); i++ ) {
+            System.out.println(myList.get(i));
+        }
     }
 
 

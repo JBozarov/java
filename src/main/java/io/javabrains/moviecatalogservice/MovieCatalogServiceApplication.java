1b6fbc2b5c1de54fa39f582d0133b46014133f37
jbozarov <tech@jbozarov.com>
2021-05-20 17:49:36 -0400

    timeout set for resttemplate callj

--- src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,8 +14,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class MovieCatalogServiceApplication {
 
   @Bean
+  @LoadBalanced
   public RestTemplate getRestTemplate() {
-    return new RestTemplate();
+    HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+    httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);
+    return new RestTemplate(httpComponentsClientHttpRequestFactory);
   }
 
   public static void main(String[] args) {
